793b55976a4af9da1bc77cc1fb3c66292d9c44f1
Edison Moreland <edison@devdude.net>
2023-11-24 22:08:40 -0800

    Initial Commit

---
+++ CreatureCreator/CreatureCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatureCreatorApp.swift
+//  CreatureCreator
+//
+//  Created by Edison Moreland on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CreatureCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
